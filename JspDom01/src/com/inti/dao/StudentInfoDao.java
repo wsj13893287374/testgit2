8510ca7880ce3451234ba8b0358a03bd7222cd0e
王世军 <1404196411@qq.com>
2021-01-19 17:20:40 +0800

    新建class-StudentInfoDao提交

---
+++ JspDom01/src/com/inti/dao/StudentInfoDao.java
@@ -0,0 +1,20 @@
+package com.inti.dao;
+/**@文件名: StudentInfoDao.java
+ * @类功能说明: 
+ * @作者: WangShiJun
+ * @Email: 1404196411@qq.com
+ * @日期: 2021年1月19日下午5:20:01
+ * @修改说明:<br> 
+ * <pre>
+ * 	 <li>作者: WangShiJun</li> 
+ * 	 <li>日期: 2021年1月19日下午5:20:01</li> 
+ *	 <li>内容: </li>
+ * </pre>
+ */
+public class StudentInfoDao {
+
+	public StudentInfoDao() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
